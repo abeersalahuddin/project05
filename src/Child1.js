6eeb22577e8701bc5c40bfbc99013ceeaf84d8b9
abeersalahuddin <52610302+abeersalahuddin@users.noreply.github.com>
2020-06-17 20:55:00 +0500

    added .js file

---
+++ src/Child1.js
@@ -0,0 +1,23 @@
+import React, { useContext } from 'react';
+import CounterContext from './CounterContext';
+
+
+
+const Child1 = () => {
+    let counterValue = useContext(CounterContext)
+
+    return (
+        <div>
+            <h2>This is the first child using Counter Context</h2>
+            <h4>Counter value is: {counterValue[0]}</h4>
+
+            <button onClick={()=> {counterValue[1](++counterValue[0])}}>
+                Increment Context
+
+            </button>
+        </div>
+    )
+}
+
+
+export default Child1;
